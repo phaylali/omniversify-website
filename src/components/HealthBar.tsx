062ce809fab43c636413d9c68a47f17d9c3a76f3
Phaylali <admin@omniversify.com>
2024-11-04 03:11:46 +0100

    adding a lot of stuff and redisgning the hp bar , adding a radar

--- src/components/HealthBar.tsx
+++ src/components/HealthBar.tsx
@@ -1,5 +1,7 @@
-import { useState, useEffect } from "react";
 
+
+
+import { useState, useEffect } from "react";
 
 function HealthBar() {
   const [health, setHealth] = useState(100);
@@ -7,31 +9,42 @@ function HealthBar() {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setHealth((prevHealth) => prevHealth - 1);
-    }, 500); // Update health every minute
+    }, 5000);
 
     return () => clearInterval(intervalId);
   }, []);
+
+  const createStrips = () => {
+    const strips = [];
+    for (let i = 0; i < health; i++) {
+      strips.push(
+        <div
+          key={i}
+          className="h-6 w-[0.4%] bg-teal-300 mr-[1px] transform skew-x-[-15deg]"
+        ></div>
+      );
+    }
+    return strips;
+  };
 
   return (
     <div className="flex justify-between w-full">
       {health > 0 ? (
         <>
-          <div
-            className={`bg-teal-300 h-6 transition-width duration-500 flex items-center`}
-            style={{ width: `${health}%` }}
-          ></div>
-          <h1 className="  text-center  text-black text-lg h-6 text-bold absolute ml-2 mb-2 pt-0">
-            {health}%
-          </h1>
+          <div className="flex w-full transform skew-x-[-15deg]">
+            {createStrips()}
+          </div>
+          
         </>
-      ) : (<div className="center-items  items-center self-center text-teal-300">
-        <center><h2 className="center-item">YDK F ZEB!</h2></center>
-        <center><p>GO TOUCH SOME GRASS.</p></center>
-        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src="https://i.ytimg.com/vi/4H6ng8fdvB8/maxresdefault.jpg" alt="HOW TO TOUCH GRASS" /></a>
-      </div>
-        
+      ) : (
+        <div className="center-items items-center self-center text-teal-300">
+          <center><h2 className="center-item">YDK F ZEB!</h2></center>
+          <center><p>GO TOUCH SOME GRASS.</p></center>
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+            <img src="https://i.ytimg.com/vi/4H6ng8fdvB8/maxresdefault.jpg" alt="HOW TO TOUCH GRASS" />
+          </a>
+        </div>
       )}
-      
     </div>
   );
 }
